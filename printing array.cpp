17ce4f1b4daa8ddb9ca9d463402425344a2198f0
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:38:07 +0530

    Create printing array.cpp

---
+++ printing array.cpp
@@ -0,0 +1,14 @@
+//Anjaneya Kalchuri
+//24070123015
+//ENTC-A1
+
+#include <iostream>
+using namespace std;
+int main() {
+    int array[]={1,3,4,5,6,7,8};
+    for(int i=0;i<7;i++){
+        cout<<array[i]<<" ";        
+    }
+
+    return 0;
+}

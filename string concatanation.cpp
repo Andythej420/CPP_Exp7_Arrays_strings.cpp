8ff22d9724771fb984dc31a333600960bcb07c0b
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:39:37 +0530

    Create string concatanation.cpp

---
+++ string concatanation.cpp
@@ -0,0 +1,19 @@
+//Anjaneya Kalchuri
+//24070123015
+//ENTC-A1
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str1, str2;
+    cout << "Enter first string: ";
+    getline(cin, str1);
+    cout << "Enter second string: ";
+    getline(cin, str2);
+    string result = str1 + str2;  
+    cout << result << endl;
+
+    return 0;
+}

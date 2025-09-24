3f3a1a97db33d8adf55edea4f90613e8120079d1
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:40:51 +0530

    Create sum and average.cpp

---
+++ sum and average.cpp
@@ -0,0 +1,18 @@
+//Anjaneya Kalchuri
+//24070123015
+//ENTC-A1
+
+#include <iostream>
+using namespace std;
+int main() {
+    int array[]={1, 3, 4, 5, 6, 7, 8};
+    int sum=0;
+    for (int i = 0; i < 7; i++) {
+        sum+=array[i];
+       
+    }  
+    float average=sum/7;
+    cout<<"The sum of elements in the array is "<< sum<<endl;
+    cout<<"The average of elements in the array is "<< average;
+    return 0;
+}

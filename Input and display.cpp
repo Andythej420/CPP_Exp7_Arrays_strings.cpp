dc8f68c2ef72776c7298d3c226e937dac18bb841
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 11:33:30 +0530

    Create Input and display.cpp

---
+++ Input and display.cpp
@@ -0,0 +1,15 @@
+//Anjaneya Kalchuri
+//24070123015
+//ENTC-A1
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    char str1[100];  
+    cout << "Enter your string: ";
+    cin.getline(str1, 100);  
+    cout <<str1 << endl;
+    return 0;
+}
